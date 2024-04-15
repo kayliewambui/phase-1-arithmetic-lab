65ef2e963474ff40aa005652ffb349948c221da1
kayliewambui <kayliewambui20@gmail.com>
2024-04-15 13:17:48 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31
+const num3 = 14;
+const num4 = 5;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+//console.log(Math.ceil(Math.random() * 10));
+const mod = num3 % num4;
+const max = Math.max(11, 12, 13, 19, 20);
+console.log(multiply);
+console.log(mod);
+console.log(random);
+console.log(max);
